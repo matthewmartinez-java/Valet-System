2cddee88c42162f0e2839c027f1423f55cec0eb3
Matthew Martinez <152852397+matthewmartinez-java@users.noreply.github.com>
2025-02-15 12:42:14 -0800

    Create Ticket.h

---
+++ Ticket.h
@@ -0,0 +1,27 @@
+#ifndef CSC340_VALET_SYSTEM_TICKET_H
+#define CSC340_VALET_SYSTEM_TICKET_H
+#include "Car.h"
+#include "Guest.h"
+class Ticket : public ValetCompany {
+public:
+    Ticket();
+    Ticket(int ticketNum);
+    int getTicketNum() const;
+    void setCarDetails(std::string make, std::string model, std::string color, std::string licensePlate);
+    void setGuestDetails(std::string name, bool isVip, int ticketNum);
+    void setGuestDetails(std::string name, bool isVip);
+    void setTicketNum(int ticketNum);
+    double getPrice();
+    void print();
+    Car getCarDetails();
+    Guest getGuestDetails();
+private:
+    int ticketNum;
+    double price;
+    Car carDetails;
+    Guest guestDetails;
+
+};
+
+
+#endif //CSC340_VALET_SYSTEM_TICKET_H
